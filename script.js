c34038ff52215021265b071244da4a49cb029359
GMMemon875 <ghulammemon875@gmail.com>
2024-04-27 19:31:34 +0500

    Now Almost Complete there is Also called Api

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const dropdown = document.querySelectorAll(".dropdown select");
+const BTN = document.querySelector("from button");
+const frmCurr = document.querySelector("#fromselect");
+const tocurr = document.querySelector("#Toselect");
 
 for (code in countryList) {
   // console.log(code, countryList[code]); // prints the code and corresponding value
@@ -30,7 +33,32 @@ const UpdateFlage = (element) => {
   let currcode = element.value; // getting current value/ value main option (PK USD) of selected option
   let countryCode = countryList[currcode]; // getting Corresponding Flag From Countrylist Object
   let FlagApi = `https://flagsapi.com/${countryCode}/flat/64.png`; // Creating new API link with help of country Code
-  // console.log(FlagApi);
+  // console.log(countryCode);
   let img = element.parentElement.querySelector("img");
   img.src = FlagApi; // setting src attribute with the help of API
 };
+
+BTN.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  let amount = document.querySelector("#amount"); //getting input field value target to id amount from Html Dom
+  let amountValue = amount.value; //getting input field value save amountvalue varible
+  // console.log(amountValue);
+  if (amountValue == "0" || amountValue == 0) {
+    amountValue == 0; // not complete this
+    amount.Value == "0";
+    alert("Please enter an amount greater than zero!");
+  }
+  // console.log(frmCurr.value, tocurr.value);
+
+  let url = `https://v6.exchangerate-api.com/v6/bbf95054d4f767f9570419fc/latest/${frmCurr.value}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      let exChangerate = data.conversion_rates[tocurr.value]; // Getting Rates Data According To Selected
+      // Your code to handle the response data goes here
+      let totalExchangerate = (amountValue * exChangerate).toFixed(2);
+      const result = document.querySelector(".result");
+      result.innerText = `${amountValue}${frmCurr.value}= ${totalExchangerate}${tocurr.value}`;
+      // console.log(result);
+    });
+});
