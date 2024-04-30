ed9bb92a396582cf406b09270ee68d730304c2ec
GMMemon875 <ghulammemon875@gmail.com>
2024-05-01 02:47:52 +0500

    little change flag change

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ for (let select of dropdown) {
     let opt = document.createElement("option"); // Dom target to selected option  and save to Opt varibale
     opt.innerText = currcode; // Dom Tareget and save  inner text as Currcode Value
     opt.value = currcode; // opt  value is same as inntertext because we are using it to compare with user input
-    if (select.name === "from" && currcode == "USA") {
+    if (select.name === "from" && currcode === "USA") {
       // using if else condition we are set the selected  Option is Default or not
       opt.selected = true;
-    } else if (select.name === "to" && currcode == "Pkr") {
+    } else if (select.name === "to" && currcode === "PkR") {
       opt.selected = false;
     }
     // // console.log(currcode);
